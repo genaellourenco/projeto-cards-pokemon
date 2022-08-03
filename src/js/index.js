744b03bac7ba27f2a33f17dab35a28a41261173b
Genael Lourenço Filho <108880124+genaellourenco@users.noreply.github.com>
2022-08-03 14:39:05 -0300

    atualizei

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const btnAvancar = document.getElementById('btn-avanca1');
+
+btnAvancar.addEventListener('click', function () {
+  const cartoes = document.querySelectorAll('.cartao-pokemon');
+});
